67f84d0ccc575328d87bff296708249057f5d3e4
RicardoCamposJr <115373284+RicardoCamposJr@users.noreply.github.com>
2023-07-25 19:17:42 -0300

    POO

--- Activity13_Blog/index.js
+++ Activity13_Blog/index.js
@@ -1,17 +1,17 @@
-const Post = require("./Post")
-const Author = require("./Author")
-const Comment = require("./Comment")
+const Post = require("./Post");
+const Author = require("./Author");
+const Comment = require("./Comment");
 
 //Criação do autor do post e o post:
-const author = new Author("Ricardo")
-const post = author.createPost()
+const author = new Author("Ricardo");
+const post = author.createPost();
 
 //Comentários no post:
-const commentAuthor = new Author("Rodrigo")
-post.addComment(commentAuthor, "Muito legal!")
-const commentAuthor1 = new Author("Sarah")
-post.addComment(commentAuthor1, "Muito legal!")
+const commentAuthor = new Author("Rodrigo");
+post.addComment(commentAuthor, "Muito legal!");
+const commentAuthor1 = new Author("Sarah");
+post.addComment(commentAuthor1, "Muito legal!");
 
 //Exibição:
-console.log(author)
-console.log(author.posts[0].comments)
+console.log(author);
+console.log(author.posts[0].comments);
